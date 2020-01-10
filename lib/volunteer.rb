0153f57adc9fd20bcf107c18d127cb164f247741
Joshua Forman <cyborgforman@gmail.com>
2020-01-10 15:20:29 -0800

    add code to pass volunteer save spec

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -4,8 +4,8 @@ class Volunteer
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
-    @project_id = attributes.fetch(:project_id)
-    @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id).to_i
+    @id = attributes.fetch(:id).to_i
   end
 
   def ==(volunteer_to_compare)
@@ -23,8 +23,13 @@ class Volunteer
       name = volunteer.fetch("name")
       project_id = volunteer.fetch("project_id")
       id = volunteer.fetch("id").to_i
-      volunteer.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
     end
     volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i
   end
 end
