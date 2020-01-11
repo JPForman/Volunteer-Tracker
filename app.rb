e59da6402a322eb64e4ec68d4e1c1814c98cb563
Joshua Forman <cyborgforman@gmail.com>
2020-01-10 16:39:13 -0800

    add get routing for index projects page with associated erb file

---
+++ app.rb
@@ -0,0 +1,18 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
